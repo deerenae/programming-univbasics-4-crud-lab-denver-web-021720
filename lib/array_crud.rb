bbb10531ae3fb92d1561bf0f16a034dedeb1e52b
Denise Magner <deerenae@gmail.com>
2020-02-05 07:15:29 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -14,6 +14,8 @@ def add_element_to_start_of_array(array, element)
   array.unshift(element)
 end
 
+# Start here need fixes
+
 def remove_element_from_end_of_array(array)
   array.pop(element)
 end
